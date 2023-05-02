cffd9d9c733a83a99d6ae0de1fd2dce32832dedd
Vineyardcode <108179228+Vineyardcode@users.noreply.github.com>
2023-05-03 00:42:03 +0200

    master

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ import { seededRandom } from 'three/src/math/MathUtils';
 
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.1, 500 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
@@ -21,7 +21,7 @@ light.position.set(0, 10, 10);
 light.castShadow = true;
 scene.add(light);
 
-const sphereGeometry = new THREE.SphereGeometry(5, 50, 50);
+const sphereGeometry = new THREE.SphereGeometry(5, 6, 100, 2.2, 5);
 sphereGeometry.translate(0,0,0)
 const material = new THREE.MeshNormalMaterial({
   flatShading: true,
@@ -34,8 +34,8 @@ sphereMesh.geometry.scale(-1, 1, 1);
 scene.add(sphereMesh); 
 
 
-  let time = 0.01;
-  const noiseScale = 5;
+  let time = 0.001;
+  const noiseScale = 1.3;
   const noise4D = createNoise4D(); 
   const noise3D = createNoise3D(); 
   const noise2D = createNoise2D(); 
@@ -43,6 +43,105 @@ scene.add(sphereMesh);
 
 
 
+
+
+
+  const heartVertices = [
+    new THREE.Vector3(0, 0, 0), // point C
+    new THREE.Vector3(0, 5, -1.5),
+    new THREE.Vector3(5, 5, 0), // point A
+    new THREE.Vector3(9, 9, 0),
+    new THREE.Vector3(5, 9, 2),
+    new THREE.Vector3(7, 13, 0),
+    new THREE.Vector3(3, 13, 0),
+    new THREE.Vector3(0, 11, 0),
+    new THREE.Vector3(5, 9, -2),
+    new THREE.Vector3(0, 8, -3),
+    new THREE.Vector3(0, 8, 3),
+    new THREE.Vector3(0, 5, 1.5), // point B
+    new THREE.Vector3(-9, 9, 0),
+    new THREE.Vector3(-5, 5, 0),
+    new THREE.Vector3(-5, 9, -2),
+    new THREE.Vector3(-5, 9, 2),
+    new THREE.Vector3(-7, 13, 0),
+    new THREE.Vector3(-3, 13, 0),
+  ];
+  const trianglesIndexes = [
+  // face 1
+    2,11,0, // This represents the 3 points A,B,C which compose the first triangle
+    2,3,4,
+    5,4,3,
+    4,5,6,
+    4,6,7,
+    4,7,10,
+    4,10,11,
+    4,11,2,
+    0,11,13,
+    12,13,15,
+    12,15,16,
+    16,15,17,
+    17,15,7,
+    7,15,10,
+    11,10,15,
+    13,11,15,
+  // face 2
+    0,1,2,
+    1,9,2,
+    9,8,2,
+    5,3,8,
+    8,3,2,
+    6,5,8,
+    7,6,8,
+    9,7,8,
+    14,17,7,
+    14,7,9,
+    14,9,1,
+    9,1,13,
+    1,0,13,
+    14,1,13,
+    16,14,12,
+    16,17,14,
+    12,14,13
+  ]
+
+
+  const heartGeometry = new THREE.BufferGeometry();
+
+  // Set the vertices
+  const hvertices = new Float32Array(heartVertices.length * 3);
+  for (let i = 0; i < heartVertices.length; i++) {
+    hvertices[i * 3] = heartVertices[i].x;
+    hvertices[i * 3 + 1] = heartVertices[i].y;
+    hvertices[i * 3 + 2] = heartVertices[i].z;
+  }
+  heartGeometry.setAttribute('position', new THREE.BufferAttribute(hvertices, 3));
+  
+  // Set the indices
+  const indices = new Uint16Array(trianglesIndexes.length);
+  for (let i = 0; i < trianglesIndexes.length; i++) {
+    indices[i] = trianglesIndexes[i];
+  }
+  heartGeometry.setIndex(new THREE.BufferAttribute(indices, 1));
+  
+  // Compute vertex normals
+  heartGeometry.computeVertexNormals();
+  
+
+
+
+
+  const hmesh = new THREE.Mesh(heartGeometry, material);
+  
+  // Add the mesh to the scene
+  scene.add(hmesh);
+
+
+
+
+
+
+  hmesh.scale.set(0.3,0.3,0.3)
+  hmesh.position.set(0,-2,0)
 
 
 
@@ -111,15 +210,18 @@ time+=0.01
 
     const position = new THREE.Vector3(x, y, z).normalize();
     const noise = noise3D(position.x* noiseScale+time, position.y* noiseScale, position.z* noiseScale);
-    position.multiplyScalar(5+0.7 * noise );
+    position.multiplyScalar(6+0.7 * noise );
     position.toArray(vertices, i);
   }
  
   sphereGeometry.computeVertexNormals();
   sphereGeometry.attributes.normal.needsUpdate = true;
   sphereGeometry.attributes.position.needsUpdate = true;
-  sphereGeometry.rotateY(0.001);
 
+
+
+  // sphereGeometry.rotateY(-0.001);
+  
   requestAnimationFrame(animate);
   renderer.render( scene, camera );
   controls.update();
